dc534b3ff3f988ee7d4848147b224878f440c6b6
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-05 20:31:50 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total
+
+  def initialize
+    @total = 0
+  end
+
+  def add_item(item)
+    title = self.item[0]
+    price = self.item[1]
+  end
+
+end
